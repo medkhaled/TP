25b016f4518b67ca8de6e6b6bbc09f12fb570986
Mohamed Khaled <medkhaledgi@gmail.com>
2025-02-26 15:07:31 +0100

    test

--- test_stats_calcul.py
+++ test_stats_calcul.py
@@ -6,7 +6,7 @@ class TestStatsCalcul(unittest.TestCase):
         self.assertEqual(somme([1, 2, 3, 4]), 10)
     
     def test_moyenne(self):
-        self.assertEqual(moyenne([1, 2, 3, 4]), 2.5)
+        self.assertEqual(moyenne([1, 2, 3, 4]), 2)
     
     def test_ecart_type(self):
         self.assertAlmostEqual(ecart_type([1, 2, 3, 4]), 1.118, places=3)
